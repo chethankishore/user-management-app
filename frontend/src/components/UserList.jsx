b352d2fa24b23cbca17cc94bf3546fee4cb03f5e
chethankishore <chethankishorecheyy77@gmail.com>
2026-03-12 22:24:06 +0530

    completed

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {useNavigate} from 'react-router'
-import { useLocation } from 'react-router'
+import { useNavigate, useLocation } from "react-router-dom";
 
 function UserList() {
 
